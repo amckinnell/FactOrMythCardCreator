cda7350fa2c2225f532b3047248671c42974b510
Alistair McKinnell <alistair.mckinnell@gmail.com>
2011-12-31 08:30:53 -0500

    Add the createPagedPdf() method to test out printing 6 Fact or Myth phrases to each page.

--- FactOrMythCardCreator/src/main/java/com/valuablecode/experiment/TableResearch.java
+++ FactOrMythCardCreator/src/main/java/com/valuablecode/experiment/TableResearch.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
+import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Phrase;
 import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.PdfPCell;
@@ -16,15 +17,19 @@ import com.itextpdf.text.pdf.PdfWriter;
 
 public class TableResearch {
 
-	public static final String RESULT = "/Users/alistair/Desktop/TableResearch.pdf";
+	public static final String INDEX_CARD_PDF = "/Users/alistair/Desktop/IndexCardTableResearch.pdf";
+	public static final String PAGED_PDF = "/Users/alistair/Desktop/PagedTableResearch.pdf";
 
     public static void main(String[] args) throws DocumentException, IOException {
-    	new TableResearch().createPdf(RESULT);
+    	TableResearch tableResearch = new TableResearch();
+    	
+		tableResearch.createIndexCardPdf(INDEX_CARD_PDF);
+		tableResearch.createPagedPdf(PAGED_PDF);
     }
  
-	public void createPdf(String filename) throws DocumentException, IOException {
-		Rectangle pagesize = new Rectangle(5 * 72, 3 * 72);	// Index card size
-		Document document = new Document(pagesize, 0f, 0f, 0f, 0f);
+	public void createIndexCardPdf(String filename) throws DocumentException, IOException {
+		Rectangle pageSize = new Rectangle(5 * 72, 3 * 72);
+		Document document = new Document(pageSize, 0f, 0f, 0f, 0f);
 
 		PdfWriter.getInstance(document, new FileOutputStream(filename));
 		
@@ -35,11 +40,38 @@ public class TableResearch {
 		c.setFixedHeight(document.getPageSize().getHeight());
 		
 		PdfPTable t = new PdfPTable(1);
+		t.setWidthPercentage(100f);
+
 		t.addCell(c);
 		
 		document.open();
 		document.add(t);
 		document.newPage();
+		document.add(t);
+		document.close();
+	}
+
+	private void createPagedPdf(String filename) throws DocumentException, IOException {
+		Document document = new Document(PageSize.LETTER, 0f, 0f, 0f, 0f);
+
+		PdfWriter.getInstance(document, new FileOutputStream(filename));
+		PdfPCell c = new PdfPCell(new Phrase("Fact or Myth Statement\n" + new Date()));
+		c.setBorder(Rectangle.NO_BORDER);
+		c.setHorizontalAlignment(Element.ALIGN_CENTER);
+		c.setVerticalAlignment(Element.ALIGN_MIDDLE);
+		c.setFixedHeight(document.getPageSize().getHeight() / 3);	// Six cells per page.
+		
+		PdfPTable t = new PdfPTable(2);
+		t.setWidthPercentage(100f);
+		
+		t.addCell(c);
+		t.addCell(c);
+		t.addCell(c);
+		t.addCell(c);
+		t.addCell(c);
+		t.completeRow();
+		
+		document.open();
 		document.add(t);
 		document.close();
 	}
