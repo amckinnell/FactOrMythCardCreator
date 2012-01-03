498a3a8101b58ba45f86d4f82ee76cf835fb17c7
Alistair McKinnell <alistair.mckinnell@gmail.com>
2012-01-03 10:12:25 -0500

    Add explanatory comment.

--- FactOrMythCardCreator/src/main/java/com/valuablecode/tool/FactOrMythLayout.java
+++ FactOrMythCardCreator/src/main/java/com/valuablecode/tool/FactOrMythLayout.java
@@ -2,6 +2,8 @@ package com.valuablecode.tool;
 
 /**
  * Allows cards to be added to a Fact or Myth card layout.
+ * 
+ * Note: the client code must indicate when they are finished adding cards by invoking the complete() method.
  */
 public interface FactOrMythLayout {
 
