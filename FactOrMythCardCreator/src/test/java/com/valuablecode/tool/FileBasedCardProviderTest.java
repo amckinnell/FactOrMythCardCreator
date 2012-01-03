66f397d802452e2803f166ef9cfc0c93380a6843
Alistair McKinnell <alistair.mckinnell@gmail.com>
2012-01-03 07:51:05 -0500

    Add explicit messages when we call the fail() method.

--- FactOrMythCardCreator/src/test/java/com/valuablecode/tool/FileBasedCardProviderTest.java
+++ FactOrMythCardCreator/src/test/java/com/valuablecode/tool/FileBasedCardProviderTest.java
@@ -2,8 +2,13 @@ package com.valuablecode.tool;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static org.junit.matchers.JUnitMatchers.containsString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.StringReader;
 import java.util.List;
 
@@ -38,6 +43,35 @@ public class FileBasedCardProviderTest {
 		assertThat(cards.size(), equalTo(2));
 		assertThat(cards.get(0).getCardText(), equalTo("First Card\nExtra Line\nExtra Extra Line"));
 		assertThat(cards.get(1).getCardText(), equalTo("Second Card"));
+	}
+	
+	@Test public void
+	fails_for_an_invalid_card_source_file_name() {
+		String invalidCardSourceFileName = "_INVALID_FILE_NAME_";
+		 
+		try {
+			new FileBasedCardProvider(invalidCardSourceFileName); 
+			fail("Expected to fail to find the card source file");
+		} catch (RuntimeException expected) {
+			assertThat(expected.getMessage(), containsString(invalidCardSourceFileName)); 
+		}
+	}
+
+	@Test public void
+	fails_for_an_invalid_card_source_file() {
+		String invalidCardSourceFileName = "_INVALID_FILE_NAME_";
+		BufferedReader reader = mock(BufferedReader.class);
+		
+		try {
+			when(reader.readLine()).thenThrow(new IOException());
+			 
+			new FileBasedCardProvider(invalidCardSourceFileName, reader); 
+			fail("Expected to fail to read a line from the BufferedReader");
+		} catch (IOException e) {
+			fail("The IO Exception should have been transformed to a RuntimeException");
+		} catch (RuntimeException expected) {
+			assertThat(expected.getMessage(), containsString(invalidCardSourceFileName)); 
+		}
 	}
 
 	private FileBasedCardProvider createFileBasedCardProvider(String cardSource) {
