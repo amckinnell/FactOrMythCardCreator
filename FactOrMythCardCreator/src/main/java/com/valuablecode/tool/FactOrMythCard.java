626e535d89e10b9d1c011df1791e4e658f0cb534
Alistair McKinnell <alistair.mckinnell@gmail.com>
2012-01-03 09:07:09 -0500

    Add a toString() method to make test failures easier to read.

--- FactOrMythCardCreator/src/main/java/com/valuablecode/tool/FactOrMythCard.java
+++ FactOrMythCardCreator/src/main/java/com/valuablecode/tool/FactOrMythCard.java
@@ -1,5 +1,8 @@
 package com.valuablecode.tool;
 
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
 /**
  * Immutable value object that knows all there is to know about a particular Fact or Myth card.
  */
@@ -15,6 +18,12 @@ public class FactOrMythCard {
 
 	public FactOrMythCard(String cardText) {
 		this.cardText = cardText;
+	}
+	
+	public String toString() {
+		return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+			.append(cardText)
+			.toString();
 	}
 
 }
