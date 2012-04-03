0e82c3a6e885690b95896f547f30e44f746e0ec6
Alistair McKinnell <alistair.mckinnell@gmail.com>
2012-04-02 21:21:09 -0400

    Use standard whitespace.

--- FactOrMythCardCreator/src/main/java/com/valuablecode/tool/itext/ConfigurableCardFormat.java
+++ FactOrMythCardCreator/src/main/java/com/valuablecode/tool/itext/ConfigurableCardFormat.java
@@ -11,15 +11,15 @@ import com.valuablecode.tool.CardFormat;
  * Knows the font to use for displaying each Fact or Myth phrase.
  */
 public class ConfigurableCardFormat implements CardFormat {
-	
+
 	// The safest choice is to embed the font in the PDF document.
 	private static final boolean EMBED_FONT_IN_DOCUMENT = true;
 
 	private final CardFontConfiguration configuration;
 
 	private Font cardFont;
-	
-	
+
+
 	public ConfigurableCardFormat(CardFontConfiguration configuration) {
 		this.configuration = configuration;
 	}
@@ -28,22 +28,22 @@ public class ConfigurableCardFormat implements CardFormat {
 		if (null == cardFont) {
 			cardFont = initializeCardFont();
 		}
-		
+
 		return cardFont;
 	}
 
 	private Font initializeCardFont() {
         Font result = createFont();
-        
+
         result.setSize(configuration.getCardFontSize());
         result.setStyle(Font.NORMAL);
-        
+
 		return result;
 	}
 
 	private Font createFont() {
         FontFactory.register(configuration.getCardFontPath());
-        
+
 		return FontFactory.getFont(configuration.getCardFontName(), defaultEncoding, EMBED_FONT_IN_DOCUMENT);
 	}
 
