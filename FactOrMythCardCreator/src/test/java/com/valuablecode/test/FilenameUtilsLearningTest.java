f32c953fa2349c992e62d4b041551769fcaa7133
Alistair McKinnell <alistair.mckinnell@gmail.com>
2012-04-09 09:22:40 -0400

    Add a learning test for Apache FilenameUtils

---
+++ FactOrMythCardCreator/src/test/java/com/valuablecode/test/FilenameUtilsLearningTest.java
@@ -0,0 +1,22 @@
+package com.valuablecode.test;
+
+import static org.apache.commons.io.FilenameUtils.getBaseName;
+import static org.apache.commons.io.FilenameUtils.getExtension;
+import static org.apache.commons.io.FilenameUtils.getPath;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class FilenameUtilsLearningTest {
+
+    final String testFilename = "/Users/alistair/ValuableCode/Clients/Shaw Media/Card Source/Offering.txt";
+
+    @Test public void
+    test() {
+        assertThat(getPath(testFilename), equalTo("Users/alistair/ValuableCode/Clients/Shaw Media/Card Source/"));
+        assertThat(getBaseName(testFilename), equalTo("Offering"));
+        assertThat(getExtension(testFilename), equalTo("txt"));
+    }
+
+}
